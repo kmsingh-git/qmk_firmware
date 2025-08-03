e09903fb90b2f40f33811eed0c27c1b964abc5aa
kmsingh-git <kmsingh245@gmail.com>
2025-08-03 11:17:08 -0400

    HRM's, except Ctrl and Alt on right. Flow Tap excl Space. Chordal Hold exc thumb clusters. Caps word. Base RGB.

--- keyboards/boardsource/unicorne/keymaps/mvrck_keymap_unicorne_6x3/keymap.c
+++ keyboards/boardsource/unicorne/keymaps/mvrck_keymap_unicorne_6x3/keymap.c
@@ -18,7 +18,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_split_3x6_3(
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,                                           KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
-        KC_ESC, LCTL_T(KC_A), LALT_T(KC_S), LGUI_T(KC_D), LSFT_T(KC_F), KC_G,           KC_H, RSFT_T(KC_J), RGUI_T(KC_K), RALT_T(KC_L), RCTL_T(KC_SCLN), KC_QUOT,
+        KC_ESC, LCTL_T(KC_A), LALT_T(KC_S), LGUI_T(KC_D), LSFT_T(KC_F), KC_G,           KC_H, RSFT_T(KC_J), RGUI_T(KC_K), KC_L, KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B,                                          KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, MO(3),
                                 KC_LCTL, MO(1), KC_LGUI,                    KC_ENT, LT(2, KC_SPACE), KC_RALT
     ),
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [3] = LAYOUT_split_3x6_3(
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                       KC_NO, KC_MUTE, KC_VOLD, KC_VOLU, KC_NO, KC_NO,
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
                                 KC_NO, KC_NO, KC_NO,                        KC_NO, KC_NO, KC_NO
     )
